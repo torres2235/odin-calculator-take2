68b008cff8466a04f5a23e759e3321c83b9843f1
Torres2235 <Torres2235@gmail.com>
2022-11-08 14:53:47 -0800

    added operate fucntion

--- js-file.js
+++ js-file.js
@@ -13,3 +13,23 @@ let mult = (x,y) => {
 let divi = (x,y) => {
     return x / y;
 }
+
+let operate = (operator, x, y) => {
+    switch(operator) {
+        case "+":
+            return add(x,y);
+            break;
+        case "-":
+            return sub(x,y);
+            break;
+        case "*":
+            return mult(x,y);
+            break;
+        case "/":
+            return div(x,y);
+            break;
+        default:
+            console.log("Invalid");
+            break;
+    }
+}
