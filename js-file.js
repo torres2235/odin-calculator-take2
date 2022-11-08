464b73f3318c889c3c4bf83caf177b9278e3a932
Torres2235 <Torres2235@gmail.com>
2022-11-08 14:43:54 -0800

    inital commit

---
+++ js-file.js
@@ -0,0 +1,15 @@
+let add = (x,y) => {
+    return x + y;
+}
+
+let sub = (x,y) => {
+    return x - y;
+}
+
+let mult = (x,y) => {
+    return x * y;
+}
+
+let divi = (x,y) => {
+    return x / y;
+}
